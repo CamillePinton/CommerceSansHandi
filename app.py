f99ba2f13c9e571b21c4b70d5e351c6dbd4fbaaa
TR17 <thomas_ruan@hotmail.fr>
2024-01-23 10:59:38 +0100

    swagger a completer

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ API_acceslibre=""
 nb_res=20
 API_Commerce="https://data.paysdelaloire.fr/api/explore/v2.1/catalog/datasets/commerces-export-openstreetmap-france/records?limit="+str(nb_res)+"&"
 
+#Documentation
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Commerce Sans Handi"
+    }
+)
+
 @app.route("/")
 def hello_world():
     return render_template('index.html')
